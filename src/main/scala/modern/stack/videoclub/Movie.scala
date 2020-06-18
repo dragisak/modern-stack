1570e7e4e38f1d35aaf080bd7c00a83791303f13
Dragisa Krsmanovic <dragisa@crunchbase.com>
2020-06-18 09:23:24 -0700

    Codec

--- src/main/scala/modern/stack/videoclub/Movie.scala
+++ src/main/scala/modern/stack/videoclub/Movie.scala
@@ -1,6 +1,13 @@
 package modern.stack.videoclub
 
+import io.circe.{Codec, Json}
+import io.circe.generic.semiauto._
+
 case class Movie(
     id: MovieId,
     title: String
 )
+
+object Movie {
+  implicit val movieCodec: Codec[Movie] = deriveCodec[Movie]
+}
--- src/main/scala/modern/stack/videoclub/package.scala
+++ src/main/scala/modern/stack/videoclub/package.scala
@@ -3,13 +3,28 @@ package modern.stack
 import java.util.UUID
 
 import eu.timepit.refined.types.numeric.PosInt
+import io.circe.{Codec, Decoder, Encoder}
 import io.estatico.newtype.macros.newtype
+
+import scala.util.Try
 
 package object videoclub {
 
   type Qty = PosInt
 
   @newtype final case class MovieId(uuid: UUID)
+
   @newtype final case class DvdId(uuid: UUID)
+
+
+  implicit val movieIdCodec: Codec[MovieId] = Codec.from(
+    Decoder.decodeString.emapTry(s => Try(MovieId(UUID.fromString(s)))),
+    Encoder.encodeString.contramap(_.uuid.toString)
+  )
+
+  implicit val dvdIdCodec: Codec[DvdId] = Codec.from(
+    Decoder.decodeString.emapTry(s => Try(DvdId(UUID.fromString(s)))),
+    Encoder.encodeString.contramap(_.uuid.toString)
+  )
 
 }
